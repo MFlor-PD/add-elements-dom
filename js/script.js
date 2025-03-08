9442a0c7d46b36b455e1f9108a06f019ec7988b0
Your Name <you@example.com>
2025-03-08 15:45:30 +0100

    planteo del ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
  // Aquí tu código
+// El boton 'AGREGAR ELEMENTO' debe hacer saltar un prompt.
+//ESE PROMPT debe estar linkeado a la lista ya creada para que al ingresar algo en el prompt se agregue a la lista
