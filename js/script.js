b84f913ee583304fdeb22e1e967c186f55f89491
Your Name <you@example.com>
2025-03-10 14:57:38 +0100

    ENTREGA FINAL, TRABAJO FUNCIONANDO

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
  // Aquí tu código
+
 const agregarLista = document.getElementById('lista');
 const botonAgregar = document.getElementById('agregar');
-
-botonAgregar.addEventListener(click, function() {
+botonAgregar.addEventListener('click', function() {
   let elemento = prompt('AGREGA ELEMENTOS A LA LISTA');
-  let li = document.createElement('li');
-  li.textContent = elemento;
-  agregarLista.appendChild(li);
-}
+ if (elemento) {
+  agregarLista.innerHTML += `<li>${elemento}</li>`;
+ }  else {
+   alert('DEBES ESCRIBIR UN ELEMENTO');
+ }
+});
+
+
